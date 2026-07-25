8555183fc8b0c612215b2a3922d37d3d0e7025de
SONU KUMAR <sonuk.gupta81@gmail.com>
2026-07-25 10:40:14 +0530

    Add Domino piling [A] from Codeforces

---
+++ 50/A - Domino piling/solution.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    int m,n;
+    cin>>m>>n;
+    int totalarea=m*n;
+    
+    int res=totalarea/2;
+    cout<<res<<endl;
+    return 0;
+}
